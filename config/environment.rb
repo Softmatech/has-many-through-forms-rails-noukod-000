675b60f18d58b06e9d2eb84bc22bba2956900424
Andy Feidje Joseph <ingandy75@gmail.com>
2019-10-22 20:39:35 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+config.public_file_server.enabled = true
+
 # Initialize the Rails application.
 Rails.application.initialize!
-config.public_file_server.enabled = true
+
